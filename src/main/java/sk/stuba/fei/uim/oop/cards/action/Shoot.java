4c1544e85df1501b449d4a78ebf9a4e2963072b0
PeterCerven <peter.cerven33@gmail.com>
2022-03-24 13:48:46 +0100

    shootPosition change number 2

--- src/main/java/sk/stuba/fei/uim/oop/cards/action/Shoot.java
+++ src/main/java/sk/stuba/fei/uim/oop/cards/action/Shoot.java
@@ -19,11 +19,12 @@ public class Shoot extends ActionCard {
 
     protected void shootPosition(int position) {
         if (this.board.get(position).getName().equals("Duck")) {
+            System.out.println(this.board.get(position).getOwner().name + "'s Duck was shot on position " +
+                    (position + 1) + ".");
+            this.board.get(position).getOwner().loseHealth();
             this.board.remove(position);
             this.board.add(boardDeck.get(0));
             this.boardDeck.remove(0);
-            System.out.println(this.board.get(position).getOwner().name + "'s Duck was shot on position " + (position + 1) + ".");
-            this.board.get(position).getOwner().loseHealth();
 
         } else {
             System.out.println(this.board.get(position).getName() + " was shoot on position " + (position + 1) + ".");
